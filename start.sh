2768dd996b0ec6ff84f82c2c90be0fc659e4e18d
ThiraphatDotSa <thiraphat_120@hotmail.com>
2025-04-15 11:27:56 +0700

    Try again

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ set -e
 
 echo "run db migration"
 export $(grep -v '^#' /app/app.env | xargs)
+echo "DB_SOURCE: $DB_SOURCE"
+echo "app.env content:"
+cat /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
